ae6db2afb256a28345e645455dfac26946187e68
AMMY-CREATE <hp4570317@gmail.com>
2026-04-13 13:09:50 +0530

    UI Enhancements: Added Desert Night Login Background, FOSSEE Logo, Updated Navigation URLs, and Project-wide Comment Cleanup

--- workshop_booking-master/workshop_app/urls.py
+++ workshop_booking-master/workshop_app/urls.py
@@ -1,18 +1,3 @@
-"""workshop_portal URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from workshop_app import views
 
@@ -25,9 +10,9 @@ urlpatterns = [
     url(r'^activate_user/$', views.activate_user),
     url(r'^login/$', views.user_login, name="login"),
     url(r'^logout/$', views.user_logout, name="logout"),
-    url(r'^status$', views.workshop_status_coordinator,
+    url(r'^status/?$', views.workshop_status_coordinator,
         name='workshop_status_coordinator'),
-    url(r'^dashboard$', views.workshop_status_instructor,
+    url(r'^dashboard/?$', views.workshop_status_instructor,
         name='workshop_status_instructor'),
     url(r'^accept_workshop/(?P<workshop_id>\d+)', views.accept_workshop,
         name='accept_workshop'),
